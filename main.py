93df225e15fa6191a83cced5b4f28da5a0b204ec
UlyanitskayaD <ulyanitskdar@gmail.com>
2019-11-03 15:44:18 +0600

    проверка связиbbbb

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def trangle(1, 5, 55,red, 60)
 #To do list: 4 figures; 2 Dasha, 2 Lida
 #Lida: square (10, 100)
 
-#Dasha: trangle (100,10)
+#Dasha: eggggs
 
-#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
 
 
