352b1c93203784c66e5256c616ae30c10072a6a0
SapinaLid <sapina.liddd@gmail.com>
2019-11-14 16:34:17 +0700

    омг квадрат
    Signed-off-by: SapinaLid <sapina.liddd@gmail.com>

--- Always-fegures.py
+++ Always-fegures.py
@@ -39,9 +39,26 @@ def parallelogram(angle0, x, y, angle1, angle2, a, b, color):
     q.penup()
 
 #Lidas def
+def square(angle0, x, y, a, color):
+    q.penup()
+    q.goto(x, y)
+    q.pendown()
+    q.setheading(angle0)
+    q.fillcolor(color)
+    q.begin_fill()
+    q.forward(a)
+    q.left(angle0)
+    q.forward(a)
+    q.left(angle0)
+    q.forward(a)
+    q.left(angle0)
+    q.forward(a)
+    q.left(angle0)
+    q.end_fill()
+    q.penup()
 
 #Lidas figures (2)
-
+square(90, -100, -100, 100, "red")
 #Dashas figures (2)
 isosceles_triangle(90, -300, 300, 100, 141.4, "yellow")
 parallelogram(90, -200, 200, 60, 120, 100, 50, "green")
